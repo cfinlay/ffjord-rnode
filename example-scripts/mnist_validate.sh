41cb8146bc5c2d02a49b3ec3da413cd23b97110c
Kinaan Aamir Khan <kkhan@wks-14-76.mpi-inf.mpg.de>
2021-05-29 16:14:17 +0200

    added path

--- example-scripts/mnist_validate.sh
+++ example-scripts/mnist_validate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python ../validate.py --data "mnist" --solver "rk4" --kinetic-energy 0.01 --jacobian-norm2 0.01 --alpha 1e-5 --test_solver dopri5 --test_atol 1e-5 --test_rtol 1e-5 --step_size 0.25 --chkpt "/HPS/CNF/work/ffjord-rnode/example-scripts/mnist/example/checkpt.pth"
+python ../validate.py --data "mnist" --solver "rk4" --kinetic-energy 0.01 --jacobian-norm2 0.01 --alpha 1e-5 --test_solver dopri5 --test_atol 1e-5 --test_rtol 1e-5 --step_size 0.25 --chkpt "/HPS/CNF/work/ffjord-rnode/experiments/mnist/example/checkpt.pth"
